1665898c57729bce24daad2648a3888a19109ec1
xRA1Nx <stwski@gmail.com>
2022-01-28 10:20:23 +0300

    рефакторинг кода

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-
-my_d = {"Почтовое отделение": (0, 2),
-        "Ул. Грибоедова, 104/25": (2, 5),
-        "Ул. Бейкер стрит, 221б": (5, 2),
-        "Ул. Большая Садовая, 302-бис": (6, 6),
-        "Вечнозелёная Аллея, 742": (8, 3),
-        }
-
+from config import my_d
 input_list = list(my_d.values())
-
 
 # поиск перебором O(n-1)!
 def shortest_way(inp_list):
@@ -20,32 +12,29 @@ def shortest_way(inp_list):
         distance_dict[str(p1) + str(p2)] = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
         temp_list2 = temp_list1.copy()
         temp_list2.remove(p2)
+        p1p2 = distance_dict[str(p1) + str(p2)]
         for p3 in temp_list2:
             distance_dict[str(p2) + str(p3)] = ((p3[0] - p2[0]) ** 2 + (p3[1] - p2[1]) ** 2) ** 0.5
             temp_list3 = temp_list2.copy()
             temp_list3.remove(p3)
+            p2p3 = p1p2 + distance_dict[str(p2) + str(p3)]
             for p4 in temp_list3:
                 distance_dict[str(p3) + str(p4)] = ((p4[0] - p3[0]) ** 2 + (p4[1] - p3[1]) ** 2) ** 0.5
                 temp_list4 = temp_list3.copy()
                 temp_list4.remove(p4)
+                p3p4 = p2p3 + distance_dict[str(p3) + str(p4)]
                 for p5 in temp_list4:
                     distance_dict[str(p4) + str(p5)] = ((p5[0] - p4[0]) ** 2 + (p5[1] - p4[1]) ** 2) ** 0.5
                     distance_dict[str(p5) + str(p1)] = ((p5[0] - p1[0]) ** 2 + (p5[1] - p1[1]) ** 2) ** 0.5
-                    p1p2 = distance_dict[str(p1) + str(p2)]
-                    p2p3 = p1p2 + distance_dict[str(p2) + str(p3)]
-                    p3p4 = p2p3 + distance_dict[str(p3) + str(p4)]
                     p4p5 = p3p4 + distance_dict[str(p4) + str(p5)]
                     p5p1 = p4p5 + distance_dict[str(p5) + str(p1)]
-                    res_dict[f"{p1} -> {p2}[{p1p2}] -> {p3}[{p2p3}] -> {p4}[{p3p4}] -> {p5}[{p4p5}] -> {p1}[{p5p1}] ="] = p5p1
-    #print(len(res_dict))
-
+                    res_dict[f"{p1} -> {p2}[{p1p2}] -> {p3}[{p2p3}] -> \
+                        {p4}[{p3p4}] -> {p5}[{p4p5}] -> {p1}[{p5p1}] ="] = p5p1
     res_way = min(res_dict, key=lambda x: res_dict[x])
     print("Кратчайший путь:")
     print(res_way, res_dict[res_way])
 
-
 # Приближенное решение с использованием жадных алгаритмов O(nlog(n):
-
 def greed_way(inp_list):
     start_point = inp_list[0]
     point1 = start_point
@@ -66,7 +55,6 @@ def greed_way(inp_list):
 
     print("\nРезультат работы 'ЖАДНОГО АЛГОРИТМА':")
     print(greed_res)
-
 
 if __name__ == '__main__':
     shortest_way(input_list)
